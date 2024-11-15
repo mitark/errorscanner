f6b22bce9027a3fe845a178feca84e8418baace6
mitark <ailimaria.tarkkala@gmail.com>
2024-11-16 00:10:33 +0200

    Create DiskSpaces.vue

---
+++ src/components/DiskSpaces.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { computed } from "vue";
+import { results } from "../useErrorScanner.js";
+
+const props = defineProps({
+  serverName: {
+    type: String,
+    required: true,
+  },
+});
+
+const disks = computed(() => results?.[props.serverName]?.diskSpace);
+</script>
+
+<template>
+  <div class="full-container">
+    <h4>Disk spaces used</h4>
+    <div class="spaces-container">
+      <div class="disk-space" v-for="disk in disks">
+        {{ disk }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.disk-space {
+  font-weight: 300;
+  font-size: 1.3em;
+
+  padding: 20px 20px;
+}
+
+.spaces-container {
+  display: flex;
+  width: 100%;
+  gap: 3rem;
+}
+
+h4 {
+  margin: 0;
+}
+
+.full-container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px 0px;
+}
+</style>
